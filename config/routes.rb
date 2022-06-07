e7fbc3907868e45794a4437ff7de3244bd0e02c6
shimaaahmedd <shimaahmedd@gmail.com>
2022-06-07 07:57:37 +0200

    modifying routes, database and removing jobs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
-Rails.application.routes.draw do
-  
-  resources :messages
+Rails.application.routes.draw do  
   post 'register', to: 'users#create'
   post 'login', to: 'authentication#login'
   get 'logout', to: 'authentication#logout'
@@ -8,8 +6,14 @@ Rails.application.routes.draw do
   delete 'delete', to: 'users#destroy'
   resources :users, :except => [:create, :update]
   resources :applications, param: :app_token do 
-    resources :chats do
-      resources :messages
+    resources :chats, param: :number do
+      member do
+        post :add_users
+      end
+      resources :messages, param: :number
+      member do
+        get :search
+      end
     end  
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
