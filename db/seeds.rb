9d649d644169247b090efe709b638f90af382c09
shimaaahmedd <shimaahmedd@gmail.com>
2022-06-07 07:56:04 +0200

    modifying seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,12 @@ puts "Starting seeding ... "
 password = 'password123'
 10.times do |n|
     first_name = Faker::Name.male_first_name
-    last_name = Faker::Name.last_name
+    second_name = Faker::Name.last_name
     email = Faker::Internet.email 
 
     user = User.create!(
         first_name: first_name,
-        last_name: last_name,
+        second_name: second_name,
         email:email,
         password: password,
     )
